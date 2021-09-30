20f07b68c025c50d109f9001690fd5f607f712c6
Marlon Aguilera <41809557+Marlon07@users.noreply.github.com>
2021-09-29 20:37:32 -0500

    Proyecto con typescript
    Creación de proyecto en react con typescript

---
+++ appcrm/src/pages/registroProductos.tsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class registroProductos extends Component {
+    render() {
+        return(
+            <h1>Registrar Producto</h1>
+        );
+    }
+}
+
+
