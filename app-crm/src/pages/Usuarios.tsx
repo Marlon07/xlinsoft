7e1e4d35cf5e4ccb86f44539b55bf7b5c500ada5
Marlon Aguilera <41809557+Marlon07@users.noreply.github.com>
2021-10-03 19:30:42 -0500

    Creación del componente menu

--- app-crm/src/pages/Usuarios.tsx
+++ app-crm/src/pages/Usuarios.tsx
@@ -1,11 +1,27 @@
 import React from 'react'
 import Menu from '../components/Menu'
+import * as Icon from 'react-feather'
+
 
 const Usuarios = () => {
     return (
         <div>
-            <Menu/>
-            <h3>Usuarios</h3>
+            <Menu />
+            {/* main */}
+            <div className="main">
+                <div className="topbar">
+                    <div className="toggle">
+                        <Icon.Menu />
+                    </div>
+                    <div className="user">
+                        <Icon.User />
+                    </div>
+                </div>
+                {/* Contenido */}
+                <div className="content">
+                    <h1>Gestionar Usuarios</h1>
+                </div>
+            </div>
         </div>
     )
 }
--- app-crm/src/pages/Ventas.tsx
+++ app-crm/src/pages/Ventas.tsx
@@ -1,11 +1,26 @@
 import React from 'react'
 import Menu from '../components/Menu'
+import * as Icon from 'react-feather'
 
 const Ventas = () => {
     return (
         <div>
             <Menu/>
-            <h3>Ventas</h3>
+            {/* main */}
+            <div className="main">
+                <div className="topbar">
+                    <div className="toggle">
+                        <Icon.Menu />
+                    </div>
+                    <div className="user">
+                        <Icon.User />
+                    </div>
+                </div>
+                {/* Contenido */}
+                <div className="content">
+                    <h1>Registrar Venta</h1>
+                </div>
+            </div>
         </div>
     )
 }
