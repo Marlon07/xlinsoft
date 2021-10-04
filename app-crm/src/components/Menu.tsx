712cf0d24ef426d471ec9b3d8e0a4d31f98de016
Marlon Aguilera <41809557+Marlon07@users.noreply.github.com>
2021-10-03 23:02:12 -0500

    Estilos del menu

--- app-crm/src/components/Menu.tsx
+++ app-crm/src/components/Menu.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 import * as Icon from 'react-feather'
 import '../assets/css/Menu.scss';
 import LogoMenu from '../assets/images/logo-menu.svg';
@@ -8,14 +8,14 @@ import LogoMenu from '../assets/images/logo-menu.svg';
 
 const Menu = () => {
 
-    let list = document.querySelectorAll('.navegacion li');
-    function activeLink(this: any) {
-        list.forEach((item) =>
-            item.classList.remove('active'));
-        this.classList.add('active');
-    }
-    list.forEach((item) =>
-        item.addEventListener('click', activeLink));
+    // const list = document.querySelectorAll('.list');
+    // function activeLink(this: any) {
+    //     list.forEach((item) =>
+    //         item.classList.remove('active'));
+    //     this.classList.add('active');
+    // }
+    // list.forEach((item) =>
+    //     item.addEventListener('click',activeLink));
 
     return (
         <div className="container">
@@ -27,31 +27,31 @@ const Menu = () => {
                             <span className="title">Xlinsoft</span>
                         </Link>
                     </li>
-                    <li className="active">
-                        <Link to="/inicio">
+                    <li className="list">
+                        <NavLink to="/inicio">
                             <span className="icon"><Icon.Home /></span>
                             <span className="title">Inicio</span>
-                        </Link>
+                        </NavLink>
                     </li>
-                    <li>
+                    <li className="list">
                         <Link to="/productos">
                             <span className="icon"><Icon.Package /></span>
                             <span className="title">Productos</span>
                         </Link>
                     </li>
-                    <li>
+                    <li className="list">
                         <Link to="/ventas">
                             <span className="icon"><Icon.ShoppingBag /></span>
                             <span className="title">Ventas</span>
                         </Link>
                     </li>
-                    <li>
+                    <li className="list">
                         <Link to="/usuarios">
                             <span className="icon"><Icon.User /></span>
                             <span className="title">Usuarios</span>
                         </Link>
                     </li>
-                    <li>
+                    <li className="list">
                         <Link to="/">
                             <span className="icon"><Icon.LogOut /></span>
                             <span className="title">Salir</span>
