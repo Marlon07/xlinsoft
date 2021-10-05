7014915a583ee50e94b759495d1c8d886bf4a908
nicolas hernandez diaz <nicolashernandezdiaz@gmail.com>
2021-10-05 18:57:02 -0500

    Interfaz Registro Ventas

--- app-crm/src/components/Contenido.tsx
+++ app-crm/src/components/Contenido.tsx
@@ -1,25 +1,26 @@
 import React from 'react'
-import Menu from '../components/Menu'
 import * as Icon from 'react-feather'
+import Inicio from '../pages/Inicio';
 
-const Productos = () => {
+const Contenido = () => {
     return (
         <div>
-            <Menu/>
             {/* main */}
             <div className="main">
                 <div className="topbar">
+                    <div className="toggle">
+                        <Icon.Menu />
+                    </div>
                     <div className="user">
                         <Icon.User />
                     </div>
                 </div>
-                {/* Contenido */}
                 <div className="content">
-                    <h1>Registrar Producto</h1>
+                    <Inicio />
                 </div>
             </div>
         </div>
     )
 }
 
-export default Productos
+export default Contenido
--- app-crm/src/components/Menu.tsx
+++ app-crm/src/components/Menu.tsx
@@ -1,47 +1,66 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import * as Icon from 'react-feather'
 import '../assets/css/Menu.scss';
+import LogoMenu from '../assets/images/logo-menu.svg';
+// import Inicio from '../pages/Inicio';
+// import Usuarios from '../pages/Usuarios';
 
 const Menu = () => {
+
+    let list = document.querySelectorAll('.navegacion li');
+    function activeLink(this: any) {
+        list.forEach((item) =>
+            item.classList.remove('active'));
+        this.classList.add('active');
+    }
+    list.forEach((item) =>
+        item.addEventListener('click', activeLink));
+
     return (
         <div className="container">
             <nav className="navegacion">
                 <ul>
                     <li>
-                        <NavLink exact to="/inicio" activeClassName="active">
-                            <span className="icon"></span>
+                        <Link to="">
+                            <span className="icon"><img src={LogoMenu} className="logo-menu" alt="logo" /></span>
                             <span className="title">Xlinsoft</span>
-                        </NavLink>
+                        </Link>
                     </li>
-                    <li>
-                        <NavLink exact to="/inicio" activeClassName="active">
+                    <li className="active">
+                        <Link to="/inicio">
                             <span className="icon"><Icon.Home /></span>
                             <span className="title">Inicio</span>
-                        </NavLink>
+                        </Link>
                     </li>
                     <li>
-                        <NavLink exact to="/productos" activeClassName="active">
+                        <Link to="/productos">
                             <span className="icon"><Icon.Package /></span>
                             <span className="title">Productos</span>
-                        </NavLink>
+                        </Link>
                     </li>
                     <li>
-                        <NavLink exact to="/ventas" activeClassName="active">
+                        <Link to="/ventas">
                             <span className="icon"><Icon.ShoppingBag /></span>
                             <span className="title">Ventas</span>
-                        </NavLink>
+                        </Link>
                     </li>
                     <li>
-                        <NavLink exact to="/usuarios" activeClassName="active">
+                        <Link to="/usuarios">
                             <span className="icon"><Icon.User /></span>
                             <span className="title">Usuarios</span>
-                        </NavLink>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/">
+                            <span className="icon"><Icon.LogOut /></span>
+                            <span className="title">Salir</span>
+                        </Link>
                     </li>
                 </ul>
             </nav>
         </div>
-    )
+    );
 }
 
 export default Menu
