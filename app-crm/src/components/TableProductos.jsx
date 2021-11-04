88514d7dd44804ca462ca9c8cdcc49814a024a17
Marlon Aguilera <41809557+Marlon07@users.noreply.github.com>
2021-11-04 01:01:09 -0500

    Funcionalidad actualización de datos

--- app-crm/src/components/TableProductos.jsx
+++ app-crm/src/components/TableProductos.jsx
@@ -5,6 +5,7 @@ import "../assets/css/css-productos/TableProductos.scss";
 import * as Icon from "react-feather";
 import { NavLink } from "react-router-dom";
 import { Modal, ModalBody, ModalFooter, ModalHeader } from "reactstrap";
+import Swal from "sweetalert2";
 
 const url = Global.baseURL;
 
@@ -12,6 +13,7 @@ class TableProductos extends Component {
   state = {
     data: [],
     modalEliminar: false,
+    modalEditar: false,
     form: {
       id: "",
       nombre: "",
@@ -20,6 +22,17 @@ class TableProductos extends Component {
       estado: "",
       tipoModal: "",
     },
+  };
+
+  modalDeleteSuccess = () => {
+    Swal.fire({
+      // text: "Producto eliminado satisfactoriamente",
+      html: "Producto eliminado satisfactoriamente",
+      timer: 2000,
+      timerProgressBar: true,
+      icon: "success",
+      confirmButtonColor: "#173e63",
+    });
   };
 
   cargarProductos = () => {
@@ -35,16 +48,17 @@ class TableProductos extends Component {
   };
 
   // ACTUALIZAR
-  // peticionPut = () => {
-  //   axios
-  //     .put(url + this.state.form.id, this.state.form)
-  //     .then((response) => {
-  //       this.cargarProductos();
-  //     })
-  //     .catch((error) => {
-  //       console.log(error.message);
-  //     });
-  // };
+  peticionPut = () => {
+    axios
+      .put(url + "/" + this.state.form.id, this.state.form)
+      .then((response) => {
+        this.setState({ modalEditar: false });
+        this.cargarProductos();
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  };
 
   peticionDelete = () => {
     axios
@@ -71,6 +85,10 @@ class TableProductos extends Component {
     });
   };
 
+  modalEditar=()=>{
+    this.setState({modalEditar: !this.state.modalEditar});
+  }
+
   handleChange = async (e) => {
     e.persist();
     await this.setState({
@@ -89,7 +107,6 @@ class TableProductos extends Component {
     const { form } = this.state;
     return (
       <div>
-
         <div>
           <span className="ico-buscar">
             <Icon.Search size={20} />
@@ -134,7 +151,7 @@ class TableProductos extends Component {
                   <td>
                     <button
                       className="btn-editar"
-                      onClick={() => this.seleccionarProducto(producto)}
+                      onClick={()=>{this.seleccionarProducto(producto); this.modalEditar()}}
                     >
                       <Icon.Edit2 size={20} />
                     </button>
@@ -154,11 +171,11 @@ class TableProductos extends Component {
           </tbody>
         </table>
 
-        <Modal className="modal" isOpen={this.state.modalEliminar}>
-          <ModalHeader>Eliminar Producto</ModalHeader>
+        <Modal isOpen={this.state.modalEliminar}>
           <span className="btn-trash">
             <Icon.Trash2 size={40} />
           </span>
+          <ModalHeader>Eliminar Producto</ModalHeader>
           <ModalBody>
             Está seguro que desea eliminar el producto {form && form.nombre}
           </ModalBody>
@@ -169,12 +186,94 @@ class TableProductos extends Component {
             >
               Cancelar
             </button>
-            <button className="btn-si" onClick={() => this.peticionDelete()}>
+            <button
+              className="btn-si"
+              onClick={() => {
+                this.peticionDelete();
+                this.modalDeleteSuccess();
+              }}
+            >
               Si
             </button>
           </ModalFooter>
         </Modal>
-        
+
+        <Modal className="modal" isOpen={this.state.modalEditar}>
+          <ModalHeader>Editar Producto</ModalHeader>
+          <ModalBody>
+          <label className="label" htmlFor="nombre">
+              Nombre
+            </label>
+            <input
+              className="input"
+              type="text"
+              name="nombre"
+              id="nombre"
+              onChange={this.handleChange}
+              value={form ? form.nombre : ""}
+              required
+            />
+
+            <label className="label" htmlFor="descripcion">
+              Descripción
+            </label>
+            <textarea
+              className="textarea"
+              type="text"
+              name="descripcion"
+              id="descripcion"
+              onChange={this.handleChange}
+              value={form ? form.descripcion : ""}
+              required
+            />
+
+            <label className="label" htmlFor="valorUnitario">
+              Valor Unitario
+            </label>
+            <input
+              className="input"
+              type="text"
+              name="valorUnitario"
+              id="valorUnitario"
+              placeholder="$"
+              onChange={this.handleChange}
+              value={form ? form.valorUnitario : ""}
+              required
+            />
+
+            <label className="label" htmlFor="estado">
+              Estado
+            </label>
+            <div className="caja">
+              <select
+                className="select"
+                id="estado"
+                name="estado"
+                onChange={this.handleChange}
+                value={form.estado}
+                required
+              >
+                <option>Seleccionar ...</option>nombre
+                <option value="Disponible">Disponible</option>
+                <option value="No disponible">No disponible</option>
+              </select>
+            </div>
+          </ModalBody>
+          <ModalFooter>
+            <button
+              className="btn-no"
+              onClick={() => this.setState({ modalEditar: false })}
+            >
+              Cancelar
+            </button>
+            <button
+              className="btn-si"
+              onClick={()=>this.peticionPut()}
+            >
+              Actualizar
+            </button>
+          </ModalFooter>
+        </Modal>
       </div>
     );
   }
